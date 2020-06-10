2063b377b5f86161cabcac730e3c064063434a52
ian <ianopolous@protonmail.com>
2020-06-10 18:08:22 +0100

    Initial implementation

---
+++ Meals.java
@@ -0,0 +1,137 @@
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Meals {
+
+    public static class Ingredient {
+        public final String name;
+        public final double amount;
+        public final String unit;
+
+        public Ingredient(String name, double amount, String unit) {
+            this.name = name;
+            this.amount = amount;
+            this.unit = unit;
+        }
+
+        @Override
+        public String toString() {
+            String value = String.format("%.1f", amount);
+            if (value.endsWith(".0"))
+                value = value.substring(0, value.length() - 2);
+            return value + " " + unit + " " + name;
+        }
+
+        public static Ingredient parse(String line) {
+            List<String> parts = Arrays.asList(line.substring(2).split(" "));
+            double amount = Double.parseDouble(parts.get(0).replaceAll("[a-zA-Z]]", ""));
+            String adjacentUnit = parts.get(0).replaceAll("[0-9\\.]", "");
+            String unit = adjacentUnit.isEmpty() ? parts.get(1) : adjacentUnit;
+            String name = (adjacentUnit.isEmpty() ? parts.stream().skip(2) : parts.stream().skip(1)).collect(Collectors.joining(" "));
+            return new Ingredient(name, amount, unit);
+        }
+    }
+    
+    public static class Recipe {
+        public final Path source;
+        public final String name;
+        public final int servings;
+        public final List<Ingredient> ingredients;
+        public final List<String> steps;
+        public final List<String> tags;
+
+        public Recipe(Path source, String name, int servings, List<Ingredient> ingredients, List<String> steps, List<String> tags) {
+            this.source = source;
+            this.name = name;
+            this.servings = servings;
+            this.ingredients = ingredients;
+            this.steps = steps;
+            this.tags = tags;
+        }
+    }
+
+    public static Recipe parse(File in) {
+        try {
+            List<String> lines = Files.readAllLines(in.toPath())
+                    .stream()
+                    .map(String::trim)
+                    .collect(Collectors.toList());
+            String name = lines.get(0).trim()
+                    .replaceFirst("title:", "")
+                    .replaceFirst("#", "");
+
+            List<String> tags = lines.stream()
+                    .filter(x -> x.startsWith("tags:"))
+                    .findFirst()
+                    .map(x -> Arrays.asList(x.split(",")))
+                    .orElse(Collections.emptyList());
+
+            int servings = lines.stream()
+                    .filter(x -> x.contains("Servings:"))
+                    .map(x -> x.substring(x.indexOf(" ", x.indexOf(":"))).trim())
+                    .map(Integer::parseInt)
+                    .findFirst()
+                    .get();
+
+            int ingredientsStart = lines.indexOf("### Ingredients") + 1;
+            int empty = (int) lines.stream().skip(ingredientsStart).takeWhile(String::isEmpty).count();
+
+            List<Ingredient> ingredients = lines.stream().skip(ingredientsStart + empty)
+                    .takeWhile(x -> !x.isEmpty())
+                    .map(Ingredient::parse)
+                    .collect(Collectors.toList());
+            int methodStart = lines.indexOf("### Method") + 1;
+            List<String> steps = lines.subList(methodStart, lines.size());
+
+            return new Recipe(in.toPath(), name, servings, ingredients, steps, tags);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<Ingredient> combine(List<Ingredient> in) {
+        Map<String, List<Ingredient>> grouped = in.stream().collect(Collectors.groupingBy(i -> i.unit + i.name));
+        return grouped.values().stream()
+                .map(same -> new Ingredient(same.get(0).name, same.stream().mapToDouble(x -> x.amount).sum(), same.get(0).unit))
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Usage: java Meals.java $recipe-dir $days $people $tag1.$tag2...$tagn");
+            return;
+        }
+        Path recipesDir = Paths.get(args[0]);
+        int days = Integer.parseInt(args[1]);
+        int people = Integer.parseInt(args[2]);
+        List<String> tags = args.length < 4 ? Collections.emptyList() : Arrays.asList(args[3].split("\\."));
+        List<Recipe> candidates = Arrays.stream(recipesDir.toFile().listFiles())
+                .filter(f -> f.getName().endsWith(".txt") || f.getName().endsWith(".md"))
+                .map(Meals::parse)
+                .filter(r -> tags.isEmpty() || r.tags.stream().anyMatch(t -> tags.stream().anyMatch(t2 -> t2.equals(t))))
+                .collect(Collectors.toList());
+        Collections.shuffle(candidates);
+
+        // make shopping list
+        int daysSoFar = 0;
+        int index = 0;
+        List<Ingredient> shoppingList = new ArrayList<>();
+        while (index < candidates.size() && daysSoFar < days) {
+            Recipe current = candidates.get(index);
+            daysSoFar += (current.servings / people);
+            index++;
+            shoppingList.addAll(current.ingredients);
+        }
+
+        System.out.println(candidates.subList(0, index).stream()
+                .map(r -> r.name)
+                .collect(Collectors.joining("\n")));
+        System.out.println("Shopping list: ");
+        System.out.println(combine(shoppingList)
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.joining("\n")));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # meal-planner
 Generate random meal plans and shopping lists from recipes
+
+# To run
+> java Meals.java $recipe-dir $days $people $tag1.$tag2...$tagn
